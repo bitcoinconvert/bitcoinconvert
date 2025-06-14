1a8386d59b48fd4c714ebea0009da2fe9b9e6cdd
bitcoinconvert <fromsouthernonline@gmail.com>
2025-06-14 08:40:12 +0300

    Futa faili zote

--- src/share-modal.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const shareModal = document.getElementById('shareModal');
-  if (shareModal) {
-    shareModal.addEventListener('click', () => {
-      shareModal.classList.toggle('hidden');
-    });
-  }
-});
